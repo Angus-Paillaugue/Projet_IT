1f10f7c18fdc142ca5e91a64bc18b020f02e3ef8
mere-patrie <angus.paillaugue40@gmail.com>
2023-04-04 13:47:03 +0200

    bite

--- public/src/main.js
+++ public/src/main.js
@@ -7,6 +7,7 @@ function main() {
     
     // Color theme
     if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) document.documentElement.classList.add('dark'); else document.documentElement.classList.remove('dark');
+    // Menu
     $(document).click((e) => {if(e.target.closest("#userMenuButton")){$($(".userMenuDropdown")[0]).toggleClass("hidden");}else if(!e.target.closest(".userMenuDropdown")){$($(".userMenuDropdown")[0]).addClass("hidden");} if(e.target.closest("#mobileMenuButton")){$("#mobileMenu").toggleClass("hidden");}else if(!e.target.closest("#mobileMenu")){$("#mobileMenu").addClass("hidden");}});
     if(getCookie("token")){$.post("/auth", {token:getCookie("token")}, (data) => {if(data.status == 200) $(".profilePicture").each(function(){$(this).attr("src", data.data.profilePicture);});});}else $(".profilePicture").each(function(){$(this).attr("src", "http://localhost/Projet_72h/public/src/defaultProfilePicture.png");});
 
